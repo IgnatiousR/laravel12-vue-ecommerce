2fe19413e9f4c71bd44ef2ade6e0d4fa6892633d
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-04 22:11:31 +0600

    Update Siderbar.vue

--- vuejs_admin/src/components/Siderbar.vue
+++ vuejs_admin/src/components/Siderbar.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="w-[200px] bg-violet-700 text-neutral-100 py-4 px-2 transition-all">
-    <img class="h-8 mb-2" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=white" alt="Your Company" />
+    <img class="h-8 mb-2 mx-2" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=white" alt="Your Company" />
 
     <router-link :to="{name: 'app.dashboard'}" class=" flex items-center gap-2 hover:bg-black/30 p-2 rounded transition-colors">
       <span><HomeIcon class="w-5"/></span>
@@ -14,10 +14,12 @@
       <span><Bars4Icon class="w-5"/></span>
       <span class=" font-semibold">Products</span>
     </router-link>
+
     <router-link :to="{name: 'app.users'}" class=" flex items-center gap-2 hover:bg-black/30 p-2 rounded transition-colors">
       <span><UsersIcon class="w-5"/></span>
       <span class=" font-semibold">Users</span>
     </router-link>
+
     <router-link :to="{name: 'app.dashboard'}" class=" flex items-center gap-2 hover:bg-black/30 p-2 rounded transition-colors">
       <span><ChartBarIcon class="w-5"/></span>
       <span class=" font-semibold">Reports</span>
