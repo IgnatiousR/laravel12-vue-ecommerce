ebc8ae495fbbc038672ed42ccacb74d5beeed421
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-04 22:11:38 +0600

    Update index.js

--- vuejs_admin/src/router/index.js
+++ vuejs_admin/src/router/index.js
@@ -7,12 +7,16 @@ import ResetPassword from "../views/ResetPassword.vue";
 import AppLayout from "../components/AppLayout.vue";
 import Products from "../views/Products.vue";
 import Users from "../views/Users.vue";
+import store from "../store";
 
 const routes = [
     {
         path: '/app',
         name: 'app',
         component: AppLayout,
+        meta: {
+            requiresAuth: true
+        },
         children: [
             {
                 path: 'dashboard',
@@ -56,6 +60,22 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+// router.beforeEach((to, from, next) => {
+//     if (!store.state.user.token){
+//         console.log(!store.state.user.token)
+//         next({ name: 'login' })
+//         // return {name: 'login'}
+//         next()
+//     } else {
+//         next()
+//     }
+// })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) next({ name: 'login' })
+  else next()
 })
 
 export default router;
