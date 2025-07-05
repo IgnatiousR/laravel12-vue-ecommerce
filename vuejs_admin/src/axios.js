69520c6c1ca327fa279ef79bca182220fb88a182
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-05 16:07:22 +0600

    Create axios.js

---
+++ vuejs_admin/src/axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import store from "./store";
+import router from "./router";
+
+const axiosCLient = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+})
+
+axiosCLient.interceptors.request.use(config => {
+    config.headers.Authorization = `Bearer ${store.replaceState.user.token}`
+    return config;
+})
+
+axiosCLient.interceptors.response.use(response => {
+    return response;
+}, error => {
+    if (error.response.status === 401) {
+        sessionStorage.removeItem('TOKEN')
+        router.push({ name: 'login'})
+    }
+
+    throw error;
+})
+
+export default axiosCLient;
