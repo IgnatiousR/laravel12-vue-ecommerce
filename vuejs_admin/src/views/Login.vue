57f3361d30661c1eb42e787daa995e31b53a11bc
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-05 21:27:39 +0600

    Update Login.vue

--- vuejs_admin/src/views/Login.vue
+++ vuejs_admin/src/views/Login.vue
@@ -4,7 +4,8 @@
             <div>
                 <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
                 <div class="mt-2">
-                    <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    <input type="email" name="email" id="email" autocomplete="email" required="" v-model="user.email"
+                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                 </div>
             </div>
 
@@ -16,8 +17,17 @@
                     </div>
                 </div>
                 <div class="mt-2">
-                    <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    <input type="password" name="password" id="password" autocomplete="current-password" required="" v-model="user.password"
+                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                 </div>
+            </div>
+
+            <div>
+                <div class="mt-2">
+                    <input type="checkbox" name="remember-me" id="remember-me" required="" v-model="user.remember"
+                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                </div>
+                <label for="email" class="block text-sm/6 font-medium text-gray-900">Remember me</label>
             </div>
 
             <div>
@@ -36,7 +46,10 @@
 
 <script setup>
 import GuestLayout from '../components/GuestLayout.vue';
-</script>
 
-<style scoped>
-</style>
+const user = {
+    email: '',
+    password: '',
+    remember: false
+}
+</script>
