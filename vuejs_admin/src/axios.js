2f1503e9ae3e19546318baf6de8b248a584f2948
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-06 22:26:29 +0600

    Update axios.js

--- vuejs_admin/src/axios.js
+++ vuejs_admin/src/axios.js
@@ -1,26 +1,27 @@
 import axios from "axios";
 import store from "./store";
-import router from "./router";
+import router from "./router/index.js";
 
-const axiosCLient = axios.create({
+const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+    //console.log(import.meta.env.VITE_API_BASE_URL);
 })
 
-axiosCLient.interceptors.request.use(config => {
+axiosClient.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${store.replaceState.user.token}`
     return config;
 })
 
-axiosCLient.interceptors.response.use(response => {
+axiosClient.interceptors.response.use(response => {
     return response;
-}, error => {
-    if (error.response.status === 401) {
-        sessionStorage.removeItem('TOKEN')
-        router.push({ name: 'login'})
-    }
+    }, error => {
+        if (error.response.status === 401) {
+            //store.commit('setToken', null)
+            sessionStorage.removeItem('TOKEN')
+            router.push({ name: 'login'})
+        }
 
     throw error;
-    // console.error(error);
 })
 
-export default axiosCLient;
+export default axiosClient;
