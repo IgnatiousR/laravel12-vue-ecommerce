821770aac869da56ce5a8021850612b5a88f9883
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-06 12:55:45 +0600

    Update Login.vue

--- vuejs_admin/src/views/Login.vue
+++ vuejs_admin/src/views/Login.vue
@@ -1,6 +1,14 @@
 <template>
     <GuestLayout title="Login to your account">
         <form class="space-y-6" method="POST" @submit.prevent="login">
+            <div class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
+                {{ errorMsg }}
+                <span @click="errorMsg = ''" 
+                class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-black/20"
+                >
+                <XMarkIcon class="w-5"/>
+                </span>
+            </div>
             <div>
                 <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
                 <div class="mt-2">
@@ -35,8 +43,18 @@
             </div>
 
             <div>
-                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >Login</button>
+                <button type="submit" :disabled="loading" 
+                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                :class="{
+                    'cursor-not-allowed': loading,
+                    'hover:bg-indigo-500': loading,
+                }"
+                >
+                <svg v-if="loading" class="mr-3 -ml-1 size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                Login</button>
             </div>
 
         </form>
@@ -49,7 +67,17 @@
 </template>
 
 <script setup>
+import { XMarkIcon } from '@heroicons/vue/24/outline';
+import { ref } from 'vue';
 import GuestLayout from '../components/GuestLayout.vue';
+import store from "../store";
+// import router from "../router";
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+let loading = ref(false);
+let errorMsg = ref("");
 
 const user = {
     email: '',
@@ -58,6 +86,15 @@ const user = {
 }
 
 function login(){
-
+    loading.value = true;
+    store.dispatch('login', user)
+    .then(() => {
+        loading.value = false;
+        router.push({ name: 'app.dashboard'})
+    })
+    .catch(({response}) => {
+        loading.value = false;
+        errorMsg.value = response.data.message;
+    })
 }
 </script>
